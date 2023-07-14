21b0dd34d3f44e3be0ff77163c7e97bad179f7c0
bearfall <84324334+bearfall@users.noreply.github.com>
2023-07-14 19:02:51 +0800

    0714

---
+++ Assets/testScript/鼠標控制系統/MouseManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+/*
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
+*/
+public class MouseManager : MonoBehaviour
+{
+    public static MouseManager Instance;
+
+    public Texture2D point, target, doorway;
+
+    RaycastHit hitInfo;     
+
+    public event Action<Vector3> OnMouseClicked;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        SetCursorTexture();
+        MouseControl();
+    }
+
+
+    void SetCursorTexture()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            switch (hitInfo.collider.gameObject.tag)
+            {
+                case "Ground":
+                    Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+                    break;
+
+            }
+
+
+        }
+
+    }
+
+    void MouseControl()
+    {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null)
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Ground"))
+            {
+                OnMouseClicked?.Invoke(hitInfo.point);
+            }
+        }
+
+
+
+
+    }
+}
